45781a1c829f6c519a627cf5bf0cd420ec63cfa5
Christophe Faribault <christophefaribault@gmail.com>
2016-04-05 14:28:53 -0400

    FOSUserBundle, admin security

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -20,6 +20,7 @@ class AppKernel extends Kernel
 
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             
+            new FOS\UserBundle\FOSUserBundle(),
             new FOS\RestBundle\FOSRestBundle(),
             
             new JMS\SerializerBundle\JMSSerializerBundle(),
